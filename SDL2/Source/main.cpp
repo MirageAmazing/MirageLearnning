a5b7c8573171a4637089d8a04eba9abbeca6d259
MirageAmazing <yangkangxierong@gmail.com>
2018-03-10 17:21:22 +0800

    平台差异代码

--- SDL2/Source/main.cpp
+++ SDL2/Source/main.cpp
@@ -1,4 +1,18 @@
-#include <SDL2/SDL.h> 
+#ifdef _WIN32
+#include "../SDL2-2.0.8/include/SDL.h"
+#include "../SDL2-2.0.8/include/SDL_main.h"
+#pragma comment(lib, "../SDL2-2.0.8/lib/x86/SDL2.lib")
+#pragma comment(lib, "../SDL2-2.0.8/lib/x86/SDL2main.lib")
+#pragma comment(lib, "../SDL2-2.0.8/lib/x86/SDL2test.lib")
+#pragma comment(linker, "/entry:SDL_main")
+#pragma comment(linker, "/INCLUDE:_mainCRTStartup")
+#define SDL_MAIN_HANDLED
+#elif __linux__
+#include <SDL2/SDL.h>
+#elif __APPLE__
+#include <SDL2/SDL.h>
+#endif
+
 #include <iostream>
 #include <chrono>
 using namespace std;
@@ -7,100 +21,99 @@ using namespace std;
 
 int Callback(void* userdata, SDL_Event* event);
 void ComputeColor(float& color, bool& control);
-long getCurrentTime();
 
 bool gGreenSwitch = false;
 double gFrames = 0;
 
-int main()  
-{  
-    float green = 0;
+int main(int argc, char *argv)
+{
+	float green = 0;
 
-    bool quit = false;
-    SDL_Event event;
-    SDL_Window* window =0;  
-    SDL_Renderer* render=0;  
-    SDL_Init(SDL_INIT_EVERYTHING);  
-    window=SDL_CreateWindow("Mirage-Application",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,640,480,SDL_WINDOW_SHOWN|SDL_WINDOW_RESIZABLE);  
-    render=SDL_CreateRenderer(window,-1,0);  
-    SDL_SetRenderDrawColor(render,0,255,0,255);  
-    SDL_RenderClear(render);  
-    SDL_RenderPresent(render); 
-    SDL_EventFilter ef = Callback; 
-    SDL_AddEventWatch((SDL_EventFilter)Callback, nullptr);
+	bool quit = false;
+	SDL_Event event;
+	SDL_Window* window = 0;
+	SDL_Renderer* render = 0;
+	SDL_Init(SDL_INIT_EVERYTHING);
+	window = SDL_CreateWindow("Mirage-Application", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+	render = SDL_CreateRenderer(window, -1, 0);
+	SDL_SetRenderDrawColor(render, 0, 255, 0, 255);
+	SDL_RenderClear(render);
+	SDL_RenderPresent(render);
+	SDL_EventFilter ef = Callback;
+	SDL_AddEventWatch((SDL_EventFilter)Callback, nullptr);
 
-    std::chrono::system_clock::time_point timeA = std::chrono::system_clock::now();
-    std::chrono::system_clock::time_point timeB = timeA;
-   
-    while( quit == false )
-    {     
-        timeB = std::chrono::system_clock::now();
-        auto diff = std::chrono::duration_cast<std::chrono::microseconds>(timeB-timeA).count();
-        gFrames = 1.0/((double)(diff)/1000000.0);
-        timeA = timeB;
+	std::chrono::system_clock::time_point timeA = std::chrono::system_clock::now();
+	std::chrono::system_clock::time_point timeB = timeA;
 
-        //If there's an event to handle
-        if( SDL_PollEvent( &event ) )
-        {
-            //If a key was pressed
-            if( event.type == SDL_KEYDOWN )
-            {
-                //Set the proper message surface
-                switch( event.key.keysym.sym )
-                {
-                    case SDLK_UP: PRINT("SDLK_UP"); PRINT(gFrames); break;
-                    case SDLK_DOWN: PRINT("SDLK_DOWN"); break;
-                    case SDLK_LEFT: PRINT("SDLK_LEFT"); break;
-                    case SDLK_RIGHT: PRINT("SDLK_RIGHT"); break;
-                }
-            }
-            else if(event.type == SDL_MOUSEBUTTONDOWN)
-            {
-                
-            }
-            //If the user has Xed out the window
-            else if( event.type == SDL_QUIT )
-            {
-                //Quit the program
-                quit = true;
-            }
-        }
-        else
-        {
-            ComputeColor(green, gGreenSwitch);
-            SDL_SetRenderDrawColor(render,0,green,0,255);  
-            SDL_RenderClear(render);  
-            SDL_RenderPresent(render); 
-        }
-    }
+	while (quit == false)
+	{
+		timeB = std::chrono::system_clock::now();
+		auto diff = std::chrono::duration_cast<std::chrono::microseconds>(timeB - timeA).count();
+		gFrames = 1.0 / ((double)(diff) / 1000000.0);
+		timeA = timeB;
 
-    SDL_DestroyWindow(window);  
-    SDL_DestroyRenderer(render);  
-    SDL_Quit();  
-    return 0;  
-} 
+		//If there's an event to handle
+		if (SDL_PollEvent(&event))
+		{
+			//If a key was pressed
+			if (event.type == SDL_KEYDOWN)
+			{
+				//Set the proper message surface
+				switch (event.key.keysym.sym)
+				{
+				case SDLK_UP: PRINT("SDLK_UP"); PRINT(gFrames); break;
+				case SDLK_DOWN: PRINT("SDLK_DOWN"); break;
+				case SDLK_LEFT: PRINT("SDLK_LEFT"); break;
+				case SDLK_RIGHT: PRINT("SDLK_RIGHT"); break;
+				}
+			}
+			else if (event.type == SDL_MOUSEBUTTONDOWN)
+			{
+
+			}
+			//If the user has Xed out the window
+			else if (event.type == SDL_QUIT)
+			{
+				//Quit the program
+				quit = true;
+			}
+		}
+		else
+		{
+			ComputeColor(green, gGreenSwitch);
+			SDL_SetRenderDrawColor(render, 0, green, 0, 255);
+			SDL_RenderClear(render);
+			SDL_RenderPresent(render);
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(render);
+	SDL_Quit();
+	return 0;
+}
 
 int Callback(void* userdata, SDL_Event* event)
 {
-    printf("\nLog on callback@@@\n");
-    return 0;
+	printf("\nLog on callback@@@\n");
+	return 0;
 }
 
 void ComputeColor(float& color, bool& control)
 {
-    if(color>255)
-    {
-        control = true;
-        color = 255;
-    }
-    else if(color < 0)
-    {
-        control = false;
-        color = 0;
-    }
+	if (color>255)
+	{
+		control = true;
+		color = 255;
+	}
+	else if (color < 0)
+	{
+		control = false;
+		color = 0;
+	}
 
-    if(control)
-        color-=0.01;
-    else
-        color+=0.01;
+	if (control)
+		color -= 0.05;
+	else
+		color += 0.05;
 }
